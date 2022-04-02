2afb1fb80e5f4daa88db7c91fb8a39c386033550
Blessed <63229016+blessed-prog@users.noreply.github.com>
2022-04-02 22:49:33 +0400

    Only extract from big files

--- pdf_file_iterator.py
+++ pdf_file_iterator.py
@@ -3,9 +3,11 @@ import os
 
 class PdfFileIterator:
 
+    # os.path.getmtime(os.path.join(address, file))
+
     def iterate(self, dir_path: str):
         tree = os.walk(dir_path)
         for address, dirs, files in tree:
             for file in files:
-                if file.endswith('.pdf'):
+                if file.endswith('.pdf') and not file.endswith('_highlighted.pdf'):
                     yield os.path.join(address, file)
