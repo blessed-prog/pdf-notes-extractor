07192d5976f62212c1eef77a1da4867474f51d43
blessed prog <63229016+blessed-prog@users.noreply.github.com>
2022-07-14 10:15:03 +0300

    Sort library by mod time

--- sort_all_files.py
+++ sort_all_files.py
@@ -1,16 +1,16 @@
-
-
 import os.path
 from datetime import datetime
 
-from highlights_extractor import HighlightsExtractor
 from pdf_file_iterator import PdfFileIterator
 
 if __name__ == "__main__":
     files = []
     iterator = PdfFileIterator()
-    for f in iterator.iterate('.'):
-        files.append(f)
-        print(datetime.fromtimestamp(os.path.getmtime(f)))
-    print(files)
+    for file in iterator.iterate('.'):
+        files.append(file)
+        print(datetime.fromtimestamp(os.path.getmtime(file)))
 
+    files.sort(key=os.path.getmtime)
+
+    for file in files:
+        print(f'{datetime.fromtimestamp(os.path.getmtime(file))}: {file}')
